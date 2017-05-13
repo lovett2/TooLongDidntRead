4b53f347804e6ab1a76b3b27f5f74574c5c88ac0
Kathryn Lovett <lovett2@uoregon.edu>
2017-05-13 12:49:19 -0700

    Updated code so that popup appears when you click browser extension icon.

--- App/background.js
+++ App/background.js
@@ -3,9 +3,24 @@
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
   // Send a message to the active tab
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+  // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  //   var activeTab = tabs[0];
+  //   chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+  // });
+
+  chrome.tabs.create({
+      url: chrome.extension.getURL('dialog.html'),
+      active: false
+  }, function(tab) {
+      // After the tab has been created, open a window to inject the tab
+      chrome.windows.create({
+          tabId: tab.id,
+          type: 'popup',
+          focused: true,
+          left: 1000,
+          width: 300
+          // incognito, top, left, ...
+      });
   });
 });
 
@@ -14,22 +29,6 @@ chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "open_new_tab" ) {
       chrome.tabs.create({"url": request.url});
-    }
-    else if (request.type === 'request_password') {
-      chrome.tabs.create({
-          url: chrome.extension.getURL('dialog.html'),
-          active: false
-      }, function(tab) {
-          // After the tab has been created, open a window to inject the tab
-          chrome.windows.create({
-              tabId: tab.id,
-              type: 'popup',
-              focused: true,
-              left: 1000,
-              width: 300
-              // incognito, top, left, ...
-          });
-      });
     }
   }
 );
