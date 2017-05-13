1ed0bb2d1c00b4ea7577c8acbf6b72802a48c9b8
Kathryn Lovett <lovett2@uoregon.edu>
2017-05-13 12:30:11 -0700

    Created basic popup that takes in password.

--- App/background.js
+++ App/background.js
@@ -15,5 +15,26 @@ chrome.runtime.onMessage.addListener(
     if( request.message === "open_new_tab" ) {
       chrome.tabs.create({"url": request.url});
     }
+    else if (request.type === 'request_password') {
+      chrome.tabs.create({
+          url: chrome.extension.getURL('dialog.html'),
+          active: false
+      }, function(tab) {
+          // After the tab has been created, open a window to inject the tab
+          chrome.windows.create({
+              tabId: tab.id,
+              type: 'popup',
+              focused: true,
+              left: 1000,
+              width: 300
+              // incognito, top, left, ...
+          });
+      });
+    }
   }
 );
+
+function setPassword(password) {
+    // Do something, eg..:
+    console.log(password);
+};
---
+++ App/dialog.js
@@ -0,0 +1,9 @@
+document.forms[0].onsubmit = function(e) {
+    e.preventDefault(); // Prevent submission
+    var password = document.getElementById('pass').value;
+    chrome.runtime.getBackgroundPage(function(bgWindow) {
+        bgWindow.setPassword(password);
+        alert(password);
+        window.close();     // Close dialog
+    });
+};
