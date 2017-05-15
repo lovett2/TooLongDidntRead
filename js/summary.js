bc53c3ea0dd2768c80ac95565c7f320802629594
Cathy Webster <cathmwebster@gmail.com>
2017-05-15 10:31:47 -0700

    Performed some tests, appears to be working as expected

--- js/summary.js
+++ js/summary.js
@@ -55,8 +55,6 @@ function get_sentences_rank(content) {
 		}
 	}
 
-	console.log(values);
-
 	// Build the sentences dictionary
 	// The score of a sentence is sum of all its intersection
 	var dict = {};
@@ -89,7 +87,6 @@ function get_best_sentence(paragraph, sentences_dic) {
 	max_value = 0;
 	for (var i = 0; i < sentences.length; i++) {
 		var s = sentences[i];
-		console.log(s);
 		strip_s = format_sentence(s);
 		if (strip_s) {
 			if (sentences_dic[strip_s] > max_value) {
@@ -110,16 +107,15 @@ function get_summary(title, content, sentences_dic) {
 	// add title
 	var summary = [];
 	summary.push(title);
-	summary.push("");
 
 	for (var i = 0; i < paragraphs.length; i++) {
 		var p = paragraphs[i];
-		sentence = get_best_sentence(p, sentences_dic).strip();
+		sentence = get_best_sentence(p, sentences_dic).trim();
 		if (sentence) {
 			summary.push(sentence);
 		}
 	}
-	return ("\n").join(summary);
+	return summary;
 }
 
 // Tool to make a 2D array with 0's
@@ -135,3 +131,16 @@ function make2DArray(n) {
     return arr;
 }
 
+/*
+title = "Swayy is a beautiful new dashboard for discovering and curating online content [Invites]";
+content = "Lior Degani, the Co-Founder and head of Marketing of Swayy, pinged me last week when I was in California to tell me about his startup and give me beta access. I heard his pitch and was skeptical. I was also tired, cranky and missing my kids – so my frame of mind wasn’t the most positive." +
+"I went into Swayy to check it out, and when it asked for access to my Twitter and permission to tweet from my account, all I could think was, “If this thing spams my Twitter account I am going to bitch-slap him all over the Internet.” Fortunately that thought stayed in my head, and not out of my mouth."
++ "One week later, I’m totally addicted to Swayy and glad I said nothing about the spam (it doesn’t send out spam tweets but I liked the line too much to not use it for this article). I pinged Lior on Facebook with a request for a beta access code for TNW readers. I also asked how soon can I write about it. It’s that good. Seriously. I use every content curation service online. It really is That Good.";
+
+sentences_dic = get_sentences_rank(content);
+summary = get_summary(title, content, sentences_dic);
+console.log(summary);
+
+console.log("Original length: ", title.length+content.length);
+console.log("Summary length: ", summary.length);
+*/
