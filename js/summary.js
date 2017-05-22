eac4014f587983d1339e0dad4cfeb1fc98c0b7df
Cathy Webster <cathmwebster@gmail.com>
2017-05-22 12:12:07 -0700

    Added how to use at the top of file

--- js/summary.js
+++ js/summary.js
@@ -1,4 +1,13 @@
 
+/*
+How to use  summarizer:
+
+sentences_dic = get_sentences_rank(content);
+summary = get_summary(title, content, sentences_dic);
+
+*/
+
+
 // method for splitting a text into sentences
 function split_content_to_sentences(content) {
 	content = content.replace("\n", ". ");
@@ -130,17 +139,3 @@ function make2DArray(n) {
 	}
     return arr;
 }
-
-/*
-title = "Swayy is a beautiful new dashboard for discovering and curating online content [Invites]";
-content = "Lior Degani, the Co-Founder and head of Marketing of Swayy, pinged me last week when I was in California to tell me about his startup and give me beta access. I heard his pitch and was skeptical. I was also tired, cranky and missing my kids – so my frame of mind wasn’t the most positive." +
-"I went into Swayy to check it out, and when it asked for access to my Twitter and permission to tweet from my account, all I could think was, “If this thing spams my Twitter account I am going to bitch-slap him all over the Internet.” Fortunately that thought stayed in my head, and not out of my mouth."
-+ "One week later, I’m totally addicted to Swayy and glad I said nothing about the spam (it doesn’t send out spam tweets but I liked the line too much to not use it for this article). I pinged Lior on Facebook with a request for a beta access code for TNW readers. I also asked how soon can I write about it. It’s that good. Seriously. I use every content curation service online. It really is That Good.";
-
-sentences_dic = get_sentences_rank(content);
-summary = get_summary(title, content, sentences_dic);
-console.log(summary);
-
-console.log("Original length: ", title.length+content.length);
-console.log("Summary length: ", summary.length);
-*/
