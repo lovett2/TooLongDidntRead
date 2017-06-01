72b206f1d45fe31f54ecfabe114a9c1b9a397cef
Meme <meme@128-223-223-202-wireless.uoregon.edu>
2017-06-01 13:52:16 -0700

    regex fix for periods in titles used with personal names being considered the end of sentences.

--- Chrome/button.js
+++ Chrome/button.js
@@ -1,8 +1,13 @@
 document.getElementById("cngButton").addEventListener("click", changeText);
 
 function changeText(){
-			var textBox = document.getElementById("summaryText");
-			sentences_dic = get_sentences_rank(textBox.innerHTML);
-			textBox.innerHTML = get_summary(textBox.innerHTML,
-											sentences_dic);
+    var textBox = document.getElementById("summaryText");
+    var toSummarize = textBox.innerHTML;
+    toSummarize = toSummarize.replace(/M(s|r|rs)\./g, function(m){
+            return {"Mr.": "Mr",
+                    "Ms.": "Ms",
+                    "Mrs.": "Mrs"}[m];
+    });
+    sentences_dic = get_sentences_rank(toSummarize);
+    textBox.innerHTML = get_summary(toSummarize,sentences_dic);
 }
