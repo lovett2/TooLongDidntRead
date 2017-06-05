b6e5c4667ec8634185d56c111890b67305c4b8ed
Cathy Webster <cathmwebster@gmail.com>
2017-06-05 10:29:31 -0700

    There were some merge conflicts that could not be automatically resolved. Fixed the conflicts manually

--- Chrome/button.js
+++ Chrome/button.js
@@ -1,7 +1,6 @@
 document.getElementById("cngButton").addEventListener("click", changeText);
 
 function changeText(){
-<<<<<<< HEAD
     var textBox = document.getElementById("summaryText");
     var toSummarize = textBox.innerHTML;
     toSummarize = toSummarize.replace(/M(s|r|rs)\./g, function(m){
@@ -11,12 +10,4 @@ function changeText(){
     });
     sentences_dic = get_sentences_rank(toSummarize);
     textBox.innerHTML = get_summary(toSummarize,sentences_dic);
-=======
-	var title = "TL;DR";
-	var textBox = document.getElementById("summaryText");
-	sentences_dic = get_sentences_rank(textBox.innerHTML);
-	textBox.innerHTML = get_summary(title,
-									textBox.innerHTML,
-									sentences_dic);
->>>>>>> firefox
 }
